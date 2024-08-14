d106837b429e5cd1be973301adf72ad1302c911a
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-08-14 21:51:01 +0400

    d

--- scripts.js
+++ scripts.js
@@ -1,34 +1,13 @@
-//  
 
-
-const audio = new Audio()
-
-function accept() {
-
-
-    const d = () => {
-        if (!audio.paused) {
-
-        }
-        else {
-            audio.src = "!more/m.mp3"
-
-            audio.play()
-
-        }
-
-    }
-    d()
-    const play = document.getElementById("play")
-    play.classList.toggle("accepted")
-
-}
 document.addEventListener('mousemove', function (e) {
     const cursor = document.querySelector('.custom-cursor');
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
 });
+// --------------
 
+let audioo = "yes"
+// title ანუ რო იცვლებოდეს
 const titles = ["DAT7EX", "DAT_7EX", "DATO", "D7EX"]
 let index = 0;
 
@@ -39,3 +18,153 @@ const dynamic = () => {
 
 }
 setInterval(dynamic, 50)
+
+// <!-- --------- -->
+fetch("https://discord.com/api/guilds/1273294291518619749/widget.json")
+    .then((e) => { return e.json() })
+    .then((user) => {
+
+        // სახელს აჭირს ანუ ასუფთავებს
+
+        const nameChecker = (name) => {
+            if (name.slice(-1) == "-" || name.slice(-1) == ".") {
+                if (name.slice(-2) == "-." || name.slice(-2) == ".-") {
+                    document.getElementById("names").innerHTML = name.slice(0, -2)
+
+                }
+                else {
+                    document.getElementById("names").innerHTML = name.slice(0, -1)
+
+                }
+
+            }
+            else {
+                document.getElementById("names").innerHTML = name
+
+            }
+        }
+        // <!-- --------- -->
+
+        // სახელს ამოწმებს და ამოქმედებს ბრძანებებს
+        const nameSimboloChecker = (name) => {
+
+            if (name.slice(-2) == "-.") {
+                document.getElementById("myVideo").style.display = "none"
+                document.getElementById("icons").style.display = "none";
+                document.getElementById("NAME").style.margin = "0px 0px 70px 0px"
+
+
+            }
+            else if (name.slice(-2) == "-,") {
+                document.getElementById("icons").style.display = "none";
+                document.getElementById("NAME").style.margin = "0px 0px 70px 0px"
+                audioo = "no"
+
+            }
+            else if (name.slice(-2) == ".,") {
+                document.getElementById("myVideo").style.display = "none"
+                audioo = "no"
+
+            }
+            else if (name.slice(-3) == "-.,") {
+                document.getElementById("myVideo").style.display = "none"
+                document.getElementById("icons").style.display = "none";
+                document.getElementById("NAME").style.margin = "0px 0px 70px 0px"
+                audioo = "no"
+
+            }
+            // --
+            else if (name.slice(-1) == "-") {
+                document.getElementById("icons").style.display = "none";
+                document.getElementById("NAME").style.margin = "0px 0px 70px 0px"
+
+            } else if (name.slice(-1) == ".") {
+                document.getElementById("myVideo").style.display = "none"
+            }
+            else if (name.slice(-1) == ",") {
+                audioo = "no"
+
+            }
+        }
+
+
+
+        // <!-- --------- -->
+        // თუ არის ონლაინზე
+        if (user && user.members[0]) {
+
+            localStorage.setItem("name", user.members[0].username)
+            // თუ არის ონლაინზე და არარის localStorage
+            if (!localStorage.getItem("name")) {
+                localStorage.setItem("name", user.members[0].username)
+                nameChecker(localStorage.getItem("name"))
+                nameSimboloChecker(localStorage.getItem("name"))
+
+
+            }
+
+            // თუ არის ონლაინზე და არის localStorage
+            else if (!!localStorage.getItem("name")) {
+                localStorage.setItem("name", user.members[0].username)
+                nameChecker(localStorage.getItem("name"))
+                nameSimboloChecker(localStorage.getItem("name"))
+
+
+
+            }
+
+        }
+        // თუ არარის ონლაინზე 
+        else {
+            // თუ არარის ონლაინზე და არის localStorage
+            if (!!localStorage.getItem("name")) {
+                nameChecker(localStorage.getItem("name"))
+                nameSimboloChecker(localStorage.getItem("name"))
+
+
+            }
+
+            // თუ არარის ონლაინზე და არარის localStorage
+            else {
+                nameChecker("DAT_7EX")
+                nameSimboloChecker("DAT_7EX")
+            }
+
+        }
+
+
+        // nameChecker(user.members[0].username)
+        // nameSimboloChecker(user.members[0].username)
+        // სიმღერის ლინკი ანუ რომელი უნდა ჩაირთოს 
+
+
+        // <!-- --------- -->
+    })
+
+function accept() {
+
+    if (audioo == "yes") {
+        const audio = new Audio()
+
+        const d = () => {
+            if (!audio.paused) {
+
+            }
+            else {
+                audio.src = "!more/sound.mp3"
+
+                audio.play()
+
+            }
+
+        }
+        d()
+        const play = document.getElementById("play")
+        play.classList.toggle("accepted")
+    } else if (audioo == "no") {
+        const play = document.getElementById("play")
+        play.classList.toggle("accepted")
+    }
+}
+
+
